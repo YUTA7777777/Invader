ea6a4598510b9ef67014ee371df77419163833fd
YUTA7777777 <YUTA@example.com>
2018-09-10 21:16:07 +0900

    Added color

--- Invader.cs
+++ Invader.cs
@@ -15,6 +15,7 @@ namespace Invader
 		{
 			Console.Clear();
 			Console.CursorVisible=false;
+			Console.Title="Invader";
 			Console.WindowWidth=32;
 			Console.WindowHeight=15;
 			p=new Player();
@@ -22,12 +23,7 @@ namespace Invader
 			p.x=0;
 			p.y=Console.WindowHeight-1;
 			p.name="A";
-			tb=new Player[8];
-			for(int i=0;i<tb.Length;i++)
-			{
-				tb[i]=new Player();
-			}
-			t=new Player[8];
+			t=new Player[9];
 			for(int i=0;i<t.Length;i++)
 			{
 				t[i]=new Player();
@@ -35,8 +31,14 @@ namespace Invader
 				t[i].y=0;
 				t[i].name="V";
 			}
+			tb=new Player[t.Length];
+			for(int i=0;i<tb.Length;i++)
+			{
+				tb[i]=new Player();
+			}
 			Run();
 			Console.Clear();
+			Console.SetCursorPosition(Console.WindowWidth/2-3,Console.WindowHeight/2);
 			if(clear)
 			{
 				Console.Write("Clear!!");
@@ -49,17 +51,6 @@ namespace Invader
 		}
 		static void Init()
 		{}
-		static void Draw()
-		{
-			for(int i=0;i<t.Length;i++)
-			{
-				t[i].x=8;
-				Console.SetCursorPosition(t[i].x,t[i].y);
-				Console.Write("V");
-			}
-			Console.SetCursorPosition(p.x,p.y);
-			Console.Write("A");
-		}
 		static void Move(int h)
 		{
 			switch(h)
@@ -110,6 +101,21 @@ namespace Invader
 						case ConsoleKey.Escape:
 							Environment.Exit(0);
 							break;
+						case ConsoleKey.A:
+						case ConsoleKey.D1:
+						case ConsoleKey.D2:
+						case ConsoleKey.D3:
+						case ConsoleKey.D4:
+						case ConsoleKey.D5:
+						case ConsoleKey.D6:
+						case ConsoleKey.D7:
+						case ConsoleKey.D8:
+							for(int i=0;i<t.Length;i++)
+							{
+								if(i==int.Parse(c.KeyChar.ToString())-1)
+								t[i].status=1;
+							}
+							break;
 						case ConsoleKey.Spacebar:
 							if(b.status!=1)
 							{
@@ -117,8 +123,10 @@ namespace Invader
 								b.x=p.x;
 								b.y=p.y-1;
 							}
+							Console.ForegroundColor=ConsoleColor.Red;
 							Console.SetCursorPosition(b.x,b.y);
 							Console.Write("|");
+							Console.ForegroundColor=ConsoleColor.White;
 							break;
 						case ConsoleKey.LeftArrow:
 							Move(1);
@@ -134,79 +142,9 @@ namespace Invader
 					}
 				}
 				TimeSpan ts = sw.Elapsed;
-				if(ts.TotalSeconds>=0.2)
+				if(ts.TotalSeconds>=0.1)
 				{
-					Console.Clear();
-					if(t[t.Length-1].x>=Console.WindowWidth-1)
-					{
-						interval=-1;
-					}else if(t[0].x<=0)
-					{
-						interval=1;
-					}
-					if(b.status==1)
-					{
-						b.y--;
-						Console.SetCursorPosition(b.x,b.y);
-						Console.Write("|");
-					}
-					int seed = Environment.TickCount;
-					for(int i=0;i<t.Length;i++)
-					{
-						t[i].x+=interval;
-						Console.SetCursorPosition(t[i].x,t[i].y);
-						if(b.y==t[i].y && b.x==t[i].x)
-							t[i].status=1;
-						if(t[i].status==0)
-						{
-							Console.Write("V");
-							if(tb[i].status==0)
-							{
-								Random r = new Random(seed++);
-								if(r.Next(4)==1)
-								{
-									tb[i].y=t[i].y;
-									tb[i].x=t[i].x;
-									tb[i].status=1;
-								}
-							}else
-							{
-								tb[i].y++;
-							}
-							if(tb[i].y==Console.WindowHeight)
-							{
-								tb[i].status=0;
-								tb[i].y=0;
-							}
-							if(tb[i].status==1)
-							{
-								Console.SetCursorPosition(tb[i].x,tb[i].y);
-								Console.Write("|");
-							}
-						}
-					}
-					int j=t.Length;
-					for(int i=0;i<t.Length;i++)
-					{
-						if(t[i].status==0)
-							clear=false;
-						else
-							j--;
-					}
-					if(j==0)
-						clear=true;
-					if(b.y==0 & b.status==1)
-					{
-						b.status=0;
-						b.y=p.y;
-					}
-					for(int i=0;i<tb.Length;i++)
-					{
-						if(p.y==tb[i].y && p.x==tb[i].x)
-							gameover=true;
-					}
-					Console.SetCursorPosition(p.x,p.y);
-					Console.Write("A");
+					Update();
 					sw.Reset();
 					sw.Start();
 				} 
@@ -216,6 +154,139 @@ namespace Invader
 						gameover=true;
 				}
 			}
+		}
+		static void Update()
+		{
+			for(int i=0;i<t.Length;i++)
+			{
+				Console.SetCursorPosition(t[i].x,t[i].y);
+				Console.Write(" ");
+			}
+			for(int i=0;i<tb.Length;i++)
+			{
+				Console.SetCursorPosition(tb[i].x,tb[i].y);
+				Console.Write(" ");
+			}
+			Console.SetCursorPosition(p.x,p.y);
+			Console.Write(" ");
+			Console.SetCursorPosition(b.x,b.y);
+			Console.Write(" ");
+			if(b.y==0 & b.status==1)
+			{
+				b.status=0;
+				b.y=p.y;
+			}
+			if(b.status==1)
+			{
+				b.y--;
+				Console.SetCursorPosition(b.x,b.y);
+				Console.ForegroundColor=ConsoleColor.Red;
+				Console.Write("|");
+				Console.ForegroundColor=ConsoleColor.White;
+			}
+			int j=t.Length;
+			for(int i=0;i<t.Length;i++)
+			{
+				if(t[i].status==0)
+					clear=false;
+				else
+					j--;
+			}
+			if(j==0)
+				clear=true;
+			else if(j>1)
+			{
+				int seed = Environment.TickCount;
+				if(t[t.Length-1].x>=Console.WindowWidth-2)
+				{
+					interval=-1;
+				}else if(t[0].x<=0)
+				{
+					interval=1;
+				}
+				for(int i=0;i<t.Length;i++)
+				{
+					t[i].x+=interval;
+					Console.SetCursorPosition(t[i].x,t[i].y);
+					if(b.y==t[i].y && b.x==t[i].x)
+						t[i].status=1;
+					if(t[i].status==0)
+					{
+						Console.Write("V");
+						if(tb[i].status==0)
+						{
+							Random r = new Random(seed++);
+							if(r.Next(j)==1)
+							{
+								tb[i].y=t[i].y+1;
+								tb[i].x=t[i].x;
+								tb[i].status=1;
+							}
+						}
+					}
+					if(tb[i].status==1)
+					{
+						tb[i].y++;
+					}
+				}
+			}
+			else if(j==1)
+			{
+				int ikinokori=0;
+				for(int i=0;i<t.Length;i++)
+				{
+					if(t[i].status==1)
+					{
+						ikinokori=i;
+					}
+				}
+				if(b.y==t[ikinokori].y && b.x==t[ikinokori].x)
+					clear=true;
+				if(interval==1 | interval==-1 | interval==2)
+				{
+					for(int k=0;k<tb.Length;k++)
+					{
+						tb[k].x=t[ikinokori].x-ikinokori+tb.Length/2+k-1;
+						tb[k].y=t[k].y+1;
+						tb[k].status=1;
+					}
+					interval=3;
+				}else if(interval>Console.WindowHeight)
+				{
+					interval=2;
+				}else
+				{
+					for(int k=0;k<tb.Length;k++)
+					{
+						tb[k].y++;
+					}
+					interval++;
+				}
+				Console.SetCursorPosition(t[ikinokori].x,t[ikinokori].y);
+				Console.Write("V");
+			}
+			for(int i=0;i<tb.Length;i++)
+			{
+				if(tb[i].y==Console.WindowHeight)
+				{
+					tb[i].status=0;
+					tb[i].y=0;
+				}
+				if(tb[i].status==1)
+				{
+					Console.SetCursorPosition(tb[i].x,tb[i].y);
+					Console.ForegroundColor=ConsoleColor.Green;
+					Console.Write("|");
+					Console.ForegroundColor=ConsoleColor.White;
+				}
+			}
+			for(int i=0;i<tb.Length;i++)
+			{
+				if(p.y==tb[i].y && p.x==tb[i].x)
+					gameover=true;
+			}
+			Console.SetCursorPosition(p.x,p.y);
+			Console.Write("A");
 		}
 	}
 	class Player
